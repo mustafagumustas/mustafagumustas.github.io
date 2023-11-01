28c80bb60b502b1c168f5a4c5ca06c1b0e55a6c7
Mustafa Gümüştaş <mustafagumustas6@gmail.com>
2023-11-01 12:27:23 +0300

    data tagle with new style

--- stock_req.py
+++ stock_req.py
@@ -1,22 +1,44 @@
 import pandas_datareader.data as web
-import pandas_datareader as pdr
+from bs4 import BeautifulSoup
 
+# Replace 'AAPL' with the desired company's symbol
+symbol = "AAPL"
+company_name = "Apple Inc."  # Add the company name here
 
-tsla = web.DataReader("TSLA", "stooq")
-appl = web.DataReader("AAPL", "stooq")
+# Load the existing HTML content
+with open("stock_data.html", "r") as html_file:
+    existing_html = html_file.read()
 
-# Modify the Python script to generate HTML content
-html_content = "<table>"
-html_content += "<tr><th>Date</th><th>Open</th><th>High</th><th>Low</th><th>Close</th><th>Volume</th></tr>"
+# Parse the existing HTML content
+soup = BeautifulSoup(existing_html, "html.parser")
 
+# Find the table element in the HTML
+table = soup.find("table")
 
-last_days_data = appl.head(7)
+# Load stock data
+stock_data = web.DataReader(symbol, "stooq")
 
+# Get the last 7 days of data and append to the table
+last_days_data = stock_data.head(7)
 for _, row in last_days_data.iterrows():
-    html_content += f"<tr><td>{row.name}</td><td>{row['Open']}</td><td>{row['High']}</td><td>{row['Low']}</td><td>{row['Close']}</td><td>{row['Volume']}</td></tr>"
+    new_row = soup.new_tag("tr")
+    new_row.append(soup.new_tag("td", text=str(row.name)))
+    new_row.append(soup.new_tag("td", text=str(row["Open"])))
+    new_row.append(soup.new_tag("td", text=str(row["High"])))
+    new_row.append(soup.new_tag("td", text=str(row["Low"])))
+    new_row.append(soup.new_tag("td", text=str(row["Close"])))
+    new_row.append(soup.new_tag("td", text=str(row["Volume"])))
+    table.append(new_row)
 
-html_content += "</table>"
+# Add the company name to the webpage
+header = soup.find("header")
+if header:
+    h1 = header.find("h1")
+    if h1:
+        h1.string = f"Stock Market Data for {company_name}"
 
-# Save the generated HTML content to the HTML file
+# Save the updated HTML content back to the HTML file
 with open("stock_data.html", "w") as html_file:
-    html_file.write(html_content)
+    html_file.write(str(soup))
+
+print(f"Stock data for {company_name} has been updated.")
